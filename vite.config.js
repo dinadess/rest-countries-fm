ddee57aa38db7973b4792e0f5f283502faf145bf
Laerice Dessouassi <laericedessouassi@gmail.com>
2023-01-03 16:40:42 +0100

    Updated config

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,17 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/rest-countries-fm/" : "/",
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(
+  (module.exports = {
+    publicPath:
+      process.env.NODE_ENV === "production" ? "/rest-countries-fm/" : "/",
+  }),
+  {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
-});
+  }
+);
